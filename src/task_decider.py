1a25512beab94dee6675e912d0b8ad16a8a04831
jamiemalcolm <jmalcolm281@gmail.com>
2020-11-26 14:57:38 +0000

    refactored test 5 and passed

--- src/task_decider.py
+++ src/task_decider.py
@@ -2,7 +2,9 @@ def get_preferred_option(task1, task2):
     if task1.description == "wash dishes" and task2.description == "cook dinner":
         return "wash dishes"
     else:
-        return "clean windows"
+        if task1.description == "clean windows" and task2.description == "wash dishes":
+            return "clean windows"
+
 # if clean window, wash dishes return clean windows
 # elif wash dishes, cook dinner return wash dishes
 #  else cook diner clean windows return cook dinner
